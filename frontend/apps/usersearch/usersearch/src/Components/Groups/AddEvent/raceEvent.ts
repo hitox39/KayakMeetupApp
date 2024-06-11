730db83064bf13c5ec3a3aefbcd7e0cd0c40f7ff
Chandler <93676693+hitox39@users.noreply.github.com>
2024-06-11 10:21:26 -0400

    chore: push old changes

---
+++ frontend/apps/usersearch/usersearch/src/Components/Groups/AddEvent/raceEvent.ts
@@ -0,0 +1,11 @@
+
+
+export default interface RaceEvent {
+    id: string,
+    eventName: string, 
+    address: string,
+    cityName: string,
+    state: number,
+    zipCode: string,
+    country: string,
+}
--- frontend/apps/usersearch/usersearch/src/Components/Groups/Events/RaceEvents.tsx
+++ frontend/apps/usersearch/usersearch/src/Components/Groups/Events/RaceEvents.tsx
@@ -1,16 +1,73 @@
-import React from 'react';
-import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table';
-import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
+import { useEffect, useState } from "react";
+import State from "../../../Models/state";
+import RaceEvent from '../AddEvent/raceEvent';
 
 
-export default function RaceEvents(){
-    return(
-        <div className=" border-2 border-stone-900 rounded-md overflow-hidden">
-            <div className=" flex flex-col md:flex-row items-center justify-center">
-                This is Race event landing
-            </div>
+export default function RaceEvents() {
+
+  const [raceEvent, setRaceEvent] = useState<RaceEvent>({
+    address: "",
+    cityName: "",
+    country: "",
+    eventName: "",
+    state: State.None,
+    id: "",
+    zipCode: "",
+  });
+
+  useEffect(() => {
+    const dataFetch = async () => {
+      var response = await fetch(`https://localhost:7062/api/fishingevent/db9e7f20-b390-450c-a171-8b28b15bdbea`, {
+        method: "GET"
+      });
+
+      const data: RaceEvent = await response.json();
+      console.log(data);
+      setRaceEvent(data);
+    };
+    dataFetch();
+
+  }, []);
+
+  return (
+    <div className='w-full my-32'>
+      <div className='max-w-[1240px] mx-auto px-2'>
+        <h2 className='text-5xl font-bold text-center'>Fishing Events</h2>
+
+
+        <div className=''>
+          <div className='flex'>
+            <table className="table-auto">
+              <thead>
+                <tr >
+                  <th>ID</th>
+                  <th className=''>Event Name</th>
+                  <th className=''> Address</th>
+                  <th className=''>City Name</th>
+                  <th className=''>State</th>
+                  <th className=''>Zip Code</th>
+                  <th>Country</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr >
+                  <td>{raceEvent.id}</td>
+                  <td>{raceEvent.eventName}</td>
+                  <td>{raceEvent.address}</td>
+                  <td>{raceEvent.cityName}</td>
+                  <td>{raceEvent.state}</td>
+                  <td>{raceEvent.zipCode}</td>
+                  <td>{raceEvent.country}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
-        
+      </div>
+    </div>
+  );
 
-    )
 }
+
+
+
